1916a5925614a8a72f818734192e0a8454771607
Eliseu Silva <eliseusilva.info@gmail.com>
2025-06-11 13:22:56 +0100

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "John"
+name = "Jane"
+name = "Liam"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
